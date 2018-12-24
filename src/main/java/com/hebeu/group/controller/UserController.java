9451dc97450658cf2eb184155c115989356f0cb3
wangyu55 <910744738@qq.com>
2018-12-24 09:02:44 +0800

    提交测试

--- src/main/java/com/hebeu/group/controller/UserController.java
+++ src/main/java/com/hebeu/group/controller/UserController.java
@@ -14,11 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserController {
     private AdminService adminService;
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test() {
-        return "测试";
-    }
+
     @Autowired
     public UserController(AdminService adminService) {
         this.adminService = adminService;
